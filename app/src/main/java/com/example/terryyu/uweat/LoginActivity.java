e7a7b21d09ea6f247df30ff3a18a930f76285119
ruitaoyu <ruitaoyu@uw.edu>
2018-12-26 22:34:33 -0800

    before database

--- app/src/main/java/com/example/terryyu/uweat/LoginActivity.java
+++ app/src/main/java/com/example/terryyu/uweat/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.example.terryyu.uweat;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        Button signUpBtn = (Button) findViewById(R.id.signUpBtn);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent SignUpIntent = new Intent (getApplicationContext(), SignUpActivity.class);
+                startActivity(SignUpIntent);
+            }
+        });
     }
 }
